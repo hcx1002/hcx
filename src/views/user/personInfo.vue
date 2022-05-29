4404377fc641e6a8a545f318fdac5bec7a9520ce
1084554414@qq.com <1084554414@qq.com>
2022-05-29 22:23:22 +0800

    整合node-scss

---
+++ src/views/user/personInfo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="content">
+        <h1 class="h1">我的信息</h1>
+        <el-button @click="toTest2">去首页</el-button>
+    </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+export default {
+    name: "PersonInfo",
+    setup () {
+        const router = useRouter()
+        const toTest2= (() => {
+            router.push("/")
+        })
+        return {
+            toTest2
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.content{
+    width: 80%;
+    margin: auto;
+    .h1{
+        color: red;
+    }
+}
+</style>
